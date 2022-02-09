d9ed3378005389eadaeb9da75e85afd3bb8e3561
sangsang6914 <sangsang6914@bandisnc.com>
2022-02-09 18:57:29 +0900

    redux first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById('add')
+const minus = document.getElementById('minus')
+const number = document.querySelector("span")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1
+  } else if (action.type === 'MINUS') {
+    return count - 1
+  }
+  return count
+}
+
+const countStore = createStore(countModifier)
+
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "MINUS"})
+countStore.dispatch({type: "MINUS"})
+countStore.dispatch({type: "MINUS"})
+
+
+console.log(countStore.getState())
